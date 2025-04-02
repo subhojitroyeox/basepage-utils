597814b44c2118cdbe8f3190e5e5c476e108a010
subhojitroyeox <subhojitroy@eoxvantage.com>
2025-04-02 21:52:06 +0530

    version_03

--- src/main/java/com/eox/utils/BasePage.java
+++ src/main/java/com/eox/utils/BasePage.java
@@ -1,9 +1,12 @@
 package com.eox.utils;
 
+import org.openqa.selenium.WebDriver;
+
 public class BasePage {
 	
-	public BasePage(int a) {
-		System.out.println(a);
+	public void openApplication(WebDriver driver) {
+		driver.get("https://github.com/subhojitroyeox/automation-utils/packages/2456167");
+		
 	}
 	
 
