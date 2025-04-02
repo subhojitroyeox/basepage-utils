fc600582fa7d38ff332a9ec32abaf5b5ff2a8472
subhojitroyeox <subhojitroy@eoxvantage.com>
2025-04-02 22:03:19 +0530

    update method type

--- src/main/java/com/eox/utils/BasePage.java
+++ src/main/java/com/eox/utils/BasePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 
 public class BasePage {
 	
-	public void openApplication(WebDriver driver) {
+	public static void openApplication(WebDriver driver) {
 		driver.get("https://github.com/subhojitroyeox/automation-utils/packages/2456167");
 		
 	}
